2144767d27bbe2aa16dfae188d7173909f7e669a
Rounak Chawla <rxc542@case.edu>
2024-10-06 07:39:48 +0530

    Scraping 200 songs per artist, initial rapper list

--- genius_scraper.py
+++ genius_scraper.py
@@ -11,10 +11,23 @@ with open('credentials', 'r') as creds:
 
 headers = {'Authorization': f'bearer {os.environ["CLIENT_ACCESS_TOKEN"]}'}
 
-URL = 'https://api.genius.com/search?q='
+URL = 'https://api.genius.com/'
 query = 'Kendrick Lamar'
 
-res = requests.get(f'{URL}{urllib.parse.quote(query)}', headers=headers)
+res = requests.get(f'{URL}search?q={urllib.parse.quote(query)}', headers=headers)
 
-with open('response', 'w+') as res_txt:
-    res_txt.write(json.dumps(res.json()))
+results = res.json()['response']['hits']
+
+# for item in results:
+#     print(f'Song title: {item["result"]["title"]} | Song ID: {item["result"]["id"]} | Artist Name: {item["result"]["primary_artist"]["name"]} | Artist ID: {item["result"]["primary_artist"]["id"]}')
+
+artist_path = results[0]['result']['primary_artist']['api_path']
+
+for i in range(1, 4):
+    params = {'sort': 'popularity',
+            'per_page': 50,
+            'page': i}
+
+    res = requests.get(f'{URL}{artist_path}/songs/', params=params, headers=headers)
+
+    results = res.json()['response']['songs']
